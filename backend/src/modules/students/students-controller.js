e9ad69225b9579d205753da0d5d3eb202f70f638
j-tap <j-tap@ya.ru>
2025-02-19 17:29:37 -0500

    feat (backend/controllers): implementation of CRUD students

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,37 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { query } = req;
+    const students = await getAllStudents(query);
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    // validation
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const payload = { ...req.body, id };
+    // validation
+    const message = await updateStudent(payload);
+    res.json(message);
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    // validation
+    const message = await setStudentStatus(payload);
+    res.json(message);
 });
 
 module.exports = {
